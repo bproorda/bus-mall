5859e70e2045a5df17f49fb8e0096504b998b4b9
bproorda <bproorda@gmail.com>
2020-03-10 13:28:32 -0500

    added chart, really wish I knew how to resize

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ var imageElements = document.getElementsByTagName('img');
 
 var totalclicks = 0;
 
-var rounds = 25;
+var rounds = 5;
 
 // var itemIndex1 = 0;
 // var itemIndex2 = 1;
@@ -75,18 +75,6 @@ for(var i = 0; i < imageElements.length; i++){
 function itemWasClicked(event) {
         totalclicks++;
         console.log(totalclicks + ' clicks');
-        // if(event.srcElement.id === '1'){
-        //     allItems[itemIndex1].timesClicked++;
-        //     // console.log('1 was clicked');
-        //   } else if (event.srcElement.id === '2'){
-        //     allItems[itemIndex2].timesClicked++;
-        //     // console.log('2 was clicked');
-        // } else if (event.srcElement.id === '3'){
-        //     allItems[itemIndex3].timesClicked++;
-        //     // console.log('3 was clicked'); 
-        //   }
-
-
 
 //attempting to rework this part for the stretch goal
 
@@ -115,45 +103,6 @@ for (var z = 0; z < imageElements.length; z++) {
     imageElements[z].src = allItems[itemIndex[z]].imageURL;
 }
 
-
-
-
-
-// //change picture after click, making sure not to repeat
-// //next Item 1
-// var nextItem1 = Math.floor(Math.random() * allItems.length);
-// while (nextItem1 === itemIndex1 || nextItem1 === itemIndex2 || nextItem1 === itemIndex3) {
-//     nextItem1 = Math.floor(Math.random() * allItems.length);
-// }
-// itemIndex1 = nextItem1;
-// allItems[itemIndex1].timesShown++;
-// console.log(itemIndex1);
-
-// //next Item 2
-// var nextItem2 = Math.floor(Math.random() * allItems.length);
-// while (nextItem2 === itemIndex1 || nextItem2 === itemIndex2 || nextItem2 === itemIndex3) {
-//     nextItem2 = Math.floor(Math.random() * allItems.length);
-// }
-// itemIndex2 = nextItem2;
-// allItems[itemIndex2].timesShown++;
-// console.log(itemIndex2)
-
-// //next Item 3
-// var nextItem3 = Math.floor(Math.random() * allItems.length);
-// while (nextItem3 === itemIndex1 || nextItem3 === itemIndex2 || nextItem3 === itemIndex3) {
-//     nextItem3 = Math.floor(Math.random() * allItems.length);
-// }
-// itemIndex3 = nextItem3;
-// allItems[itemIndex3].timesShown++;
-// console.log(itemIndex3);
-
-
-// //display images 
-// imageElements[0].src = allItems[itemIndex1].imageURL;
-// imageElements[1].src = allItems[itemIndex2].imageURL;
-// imageElements[2].src = allItems[itemIndex3].imageURL;
-
-
 //removing event listener when max rounds reached
 if (totalclicks === rounds) {
     for(var i = 0; i < imageElements.length; i++){
@@ -171,10 +120,59 @@ if (totalclicks === rounds) {
  }
 
  
-
+ createChart();
 //end of if statment, long isn't it?  
 }
 
 
 //end of function curly bracket
+}
+
+
+function findTheProperty(nameOfTheProperty) {
+    var answer = [];
+    for (var i = 0; i < allItems.length; i++) {
+        answer[i] = allItems[i][nameOfTheProperty]
+    }
+    return answer;
+}
+
+function createChart() {
+    var ctx = document.getElementById('myChart').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: findTheProperty('name'),
+        datasets: [{
+            label: '# of Votes',
+            data: findTheProperty('timesClicked'),
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
 }
